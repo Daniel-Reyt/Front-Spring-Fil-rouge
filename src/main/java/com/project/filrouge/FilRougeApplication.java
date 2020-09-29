18cf69dc18da3177e4793eddc30d009922ae4ec0
daniel.reyt <daniel-david.reyt@uha.fr>
2020-09-29 14:52:27 +0200

    spring api fonctionelle + swagger doc

--- src/main/java/com/project/filrouge/FilRougeApplication.java
+++ src/main/java/com/project/filrouge/FilRougeApplication.java
@@ -3,8 +3,10 @@ package com.project.filrouge;
 import com.project.filrouge.models.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FilRougeApplication {
 
 	public static void main(String[] args) {
